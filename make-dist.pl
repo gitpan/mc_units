8b390b81f513f81619edef2b008d367c0f17eb62
H.Merijn Brand <h.m.brand@xs4all.nl>
2007-04-13 11:09:06 +0000

    Import of HMBRAND/mc_units-20070413 from CPAN.
    gitpan-cpan-distribution: mc_units
    gitpan-cpan-version:      20070413
    gitpan-cpan-path:         HMBRAND/mc_units-20070413.tgz
    gitpan-cpan-author:       HMBRAND
    gitpan-cpan-maturity:     released

--- make-dist.pl
+++ make-dist.pl
@@ -11,14 +11,10 @@ chdir "/pro/3gl/CPAN";
 my @today = localtime;
 my $today = (($today[5] + 1900) * 100 + $today[4] + 1) * 100 + $today[3];
 
-my @mc = (
-    "bin/diff2unit.pl",
-    "bin/dual.pl",
-    "bin/metagrep",
-    "bin/patch2p4");
+my @mc;
 find (sub { -d or push @mc, $File::Find::name }, "metaconfig");
 
 my $tgz = "/tmp/mc_units-$today.tgz";
 unlink $tgz;
-Archive::Tar->create_archive ($tgz, 9, @mc);
+Archive::Tar->create_archive ($tgz, 9, sort @mc);
 print $tgz, "\t", -s $tgz, "\n";
